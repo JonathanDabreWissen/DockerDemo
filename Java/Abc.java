f25372a5c506048530c8ba1f16c88ae958134ba4
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-02-14 13:05:40 +0530

    Add Dockerfiles for Redis and Java applications with initial setup

---
+++ Java/Abc.java
@@ -0,0 +1,12 @@
+// package Java;
+
+public class Abc {
+    public static void main(String[] args) {
+        System.out.println("Hello from java program");
+        System.out.println("This is compiled through docker");
+        System.out.println("Also we have set the environment variables");
+        System.out.println("And finally executed as well");
+        System.out.println("--------------Thank You---------------");
+    }
+    
+}
